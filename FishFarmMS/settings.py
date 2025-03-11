867c13919debd1d3b687f6502ec4d28b90e4f59e
ephrim-s <ephrims5@gmail.com>
2025-03-11 18:42:37 +0000

    add postgresdb configration

--- FishFarmMS/settings.py
+++ FishFarmMS/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'FishFarmMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fishfarm_db',
+        'USER': 'postgres',
+        'PASSWORD': 'theman',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ROLE': 'FishFarmMS',
     }
 }
 
