97a69d21925afdba123a14de25976d873e6774cb
ephrim-s <ephrims5@gmail.com>
2025-03-22 20:02:32 +0000

    updated

--- farmer_dashboard/urls.py
+++ farmer_dashboard/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import ContractViewSet, PondViewSet, PondRentalViewSet
 
 router = DefaultRouter()
-router.register(r'ponds', PondViewSet)
+router.register(r'', PondViewSet)
 router.register(r'pond-rentals', PondRentalViewSet)
 router.register(r'contracts', ContractViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),  
+    path('', include(router.urls)),  
 ]
--- fishfarmms/urls.py
+++ fishfarmms/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('core.urls')),
-    path('', include('farmer.urls')),
+    path('', include('farmer_dashboard.urls')),
 ]
