2a1147a871109a60acc58a60b0dc65426042c251
ephrim-s <ephrims5@gmail.com>
2025-04-04 01:06:36 +0000

    updated settgins

--- fishfarmms/settings.py
+++ fishfarmms/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
